041b16701df94391a14dc26f5e7729bd60f1ed71
Philipp Duck <phil.mar.du@gmail.com>
2022-01-30 21:31:36 +0100

    added new Buttons and UI optimazations
    added '(,),AC,C'
    added Error Messege if calc goes wrong

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 function add(value) {
     document.getElementById('resultArea').innerHTML += value;
+    document.getElementById('messageArea').innerHTML = "";
 }
 
 function calculate() {
-    let result = eval(document.getElementById('resultArea').innerText);
-    document.getElementById('resultArea').innerHTML = result;
+    try {
+        let result = eval(document.getElementById('resultArea').innerText);
+        document.getElementById('resultArea').innerHTML = result;
+    } catch (error) {
+        document.getElementById('messageArea').innerHTML = "Fehler";
+    }
+    
+    
 }
 
-function del() {
+function delLast() {
     let container = document.getElementById('resultArea')
     container.innerHTML = container.innerHTML.slice(0, -1);
+    document.getElementById('messageArea').innerHTML = "";
+}
+
+function delAll() {
+    document.getElementById('resultArea').innerHTML = "";
+    document.getElementById('messageArea').innerHTML = "";
 }
